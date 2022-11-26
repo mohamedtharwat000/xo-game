3b46398ab0837615a85cdc796e099b4de3d52cea
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2022-11-26 19:55:47 +0200

    Update game1h.js

--- assets/scripts/game1h.js
+++ assets/scripts/game1h.js
@@ -255,29 +255,17 @@ function nextMove() {
                             matrixButtons[a][b][1].dataset.priority = (2 + Number(matrixButtons[a][b][1].dataset.priority)).toString();
                             matrixButtons[a][b][2].dataset.priority = (1 + Number(matrixButtons[a][b][2].dataset.priority)).toString();
                         }
-                        if(matrixValues[a][b][0] !== '' && matrixValues[a][b][1] !== '') {
-                            matrixButtons[a][b][2].dataset.priority = (2 + Number(matrixButtons[a][b][1].dataset.priority)).toString();
-                        }
                         break;
                     case '1':
                         if(matrixValues[a][b][1] !== '') {
                             matrixButtons[a][b][0].dataset.priority = (1 + Number(matrixButtons[a][b][0].dataset.priority)).toString();
                             matrixButtons[a][b][2].dataset.priority = (1 + Number(matrixButtons[a][b][2].dataset.priority)).toString();
                         }
-                        if(matrixValues[a][b][1] !== '' && matrixValues[a][b][0] !== '') {
-                            matrixButtons[a][b][2].dataset.priority = (2 + Number(matrixButtons[a][b][1].dataset.priority)).toString();
-                        }
-                        if(matrixValues[a][b][1] !== '' && matrixValues[a][b][2] !== '') {
-                            matrixButtons[a][b][0].dataset.priority = (2 + Number(matrixButtons[a][b][1].dataset.priority)).toString();
-                        }
                         break;
                     case '2':
                         if(matrixValues[a][b][2] !== '') {
                             matrixButtons[a][b][0].dataset.priority = (1 + Number(matrixButtons[a][b][0].dataset.priority)).toString();
                             matrixButtons[a][b][1].dataset.priority = (2 + Number(matrixButtons[a][b][1].dataset.priority)).toString();
-                        }
-                        if(matrixValues[a][b][2] !== '' && matrixValues[a][b][1] !== '') {
-                            matrixButtons[a][b][0].dataset.priority = (2 + Number(matrixButtons[a][b][1].dataset.priority)).toString();
                         }
                         break;
                 }
