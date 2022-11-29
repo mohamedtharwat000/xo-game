2bca93f5858f12b8b1a9e46e78823e12e9f383f7
Muhammad Shoaib <107323928+melsayedshoaib@users.noreply.github.com>
2022-11-29 06:58:08 +0200

    modified-single-player-easy

--- assets/scripts/single.js
+++ assets/scripts/single.js
@@ -1,131 +1,156 @@
-const selectBox = document.querySelector(".select-box"),
-  selectBtnX = selectBox.querySelector(".options .playerX"),
-  selectBtnO = selectBox.querySelector(".options .playerO"),
-  playBoard = document.querySelector(".play-board"),
-  players = document.querySelector(".players"),
-  allBox = document.querySelectorAll("section span"),
-  resultBox = document.querySelector(".result-box"),
-  wonText = resultBox.querySelector(".won-text"),
-  replayBtn = resultBox.querySelector("button");
-window.onload = () => {
-  for (let i = 0; i < allBox.length; i++) {
-    allBox[i].setAttribute("onclick", "clickedBox(this)");
+let isStarted = false;
+let isPlayer = true;
+let block = $(".single");
+block.text("");
+let display = $("#infodisplay");
+let playercode = "";
+display.text("Choose Your Letter");
+$("#chooseX").click(function () {
+  playercode = "X";
+  $(".playerchoice").addClass("hidden");
+  $(".game-grid").removeClass("hidden");
+  display.text("");
+  isStarted = true;
+});
+$("#chooseO").click(function () {
+  playercode = "O";
+  $(".playerchoice").addClass("hidden");
+  $(".game-grid").removeClass("hidden");
+  display.text("");
+  isStarted = true;
+});
+let gamespace = [
+  $("#11"),
+  $("#12"),
+  $("#13"),
+  $("#21"),
+  $("#22"),
+  $("#23"),
+  $("#31"),
+  $("#32"),
+  $("#33"),
+];
+function toggleplayer() {
+  if (playercode == "X") {
+    playercode = "O";
+  } else if (playercode == "O") {
+    playercode = "X";
   }
-};
-selectBtnX.onclick = () => {
-  selectBox.classList.add("hide");
-  playBoard.classList.add("show");
-};
-selectBtnO.onclick = () => {
-  selectBox.classList.add("hide");
-  playBoard.classList.add("show");
-  players.setAttribute("class", "players active player");
-};
-let playerXIcon = "fas fa-times",
-  playerOIcon = "far fa-circle",
-  playerSign = "X",
-  runBot = true;
-function clickedBox(element) {
-  if (players.classList.contains("player")) {
-    playerSign = "O";
-    element.innerHTML = `<i class="${playerOIcon}"></i>`;
-    players.classList.remove("active");
-    element.setAttribute("id", playerSign);
-  } else {
-    element.innerHTML = `<i class="${playerXIcon}"></i>`;
-    element.setAttribute("id", playerSign);
-    players.classList.add("active");
-  }
-  selectWinner();
-  element.style.pointerEvents = "none";
-  playBoard.style.pointerEvents = "none";
-  let randomTimeDelay = (Math.random() * 1000 + 200).toFixed();
-  setTimeout(() => {
-    bot(runBot);
-  }, randomTimeDelay);
-  console.log(playBoard);
-}
-function bot() {
-  let array = [];
-  if (runBot) {
-    playerSign = "O";
-    for (let i = 0; i < allBox.length; i++) {
-      if (allBox[i].childElementCount == 0) {
-        array.push(i);
-      }
-    }
-    let randomBox = array[Math.floor(Math.random() * array.length)];
-    if (array.length > 0) {
-      if (players.classList.contains("player")) {
-        playerSign = "X";
-        allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
-        allBox[randomBox].setAttribute("id", playerSign);
-        players.classList.add("active");
-      } else {
-        allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`;
-        players.classList.remove("active");
-        allBox[randomBox].setAttribute("id", playerSign);
-      }
-      selectWinner();
-    }
-    allBox[randomBox].style.pointerEvents = "none";
-    playBoard.style.pointerEvents = "auto";
-    playerSign = "X";
+  if (isPlayer == true) {
+    isPlayer = false;
+  } else if (isPlayer == false) {
+    isPlayer = true;
   }
 }
-function getIdVal(classname) {
-  return document.querySelector(".box" + classname).id;
-}
-function checkIdSign(val1, val2, val3, sign) {
+function checkwin() {
   if (
-    getIdVal(val1) == sign &&
-    getIdVal(val2) == sign &&
-    getIdVal(val3) == sign
+    (gamespace[0].text() == "X" &&
+      gamespace[1].text() == "X" &&
+      gamespace[2].text() == "X") ||
+    (gamespace[3].text() == "X" &&
+      gamespace[4].text() == "X" &&
+      gamespace[5].text() == "X") ||
+    (gamespace[6].text() == "X" &&
+      gamespace[7].text() == "X" &&
+      gamespace[8].text() == "X") ||
+    (gamespace[0].text() == "X" &&
+      gamespace[3].text() == "X" &&
+      gamespace[6].text() == "X") ||
+    (gamespace[1].text() == "X" &&
+      gamespace[4].text() == "X" &&
+      gamespace[7].text() == "X") ||
+    (gamespace[2].text() == "X" &&
+      gamespace[5].text() == "X" &&
+      gamespace[8].text() == "X") ||
+    (gamespace[0].text() == "X" &&
+      gamespace[4].text() == "X" &&
+      gamespace[8].text() == "X") ||
+    (gamespace[2].text() == "X" &&
+      gamespace[4].text() == "X" &&
+      gamespace[6].text() == "X")
   ) {
-    return true;
-  }
-  return false;
-}
-function selectWinner() {
-  if (
-    checkIdSign(1, 2, 3, playerSign) ||
-    checkIdSign(4, 5, 6, playerSign) ||
-    checkIdSign(7, 8, 9, playerSign) ||
-    checkIdSign(1, 4, 7, playerSign) ||
-    checkIdSign(2, 5, 8, playerSign) ||
-    checkIdSign(3, 6, 9, playerSign) ||
-    checkIdSign(1, 5, 9, playerSign) ||
-    checkIdSign(3, 5, 7, playerSign)
+    isPlayer = true;
+    isStarted = false;
+    display.text("X wins!");
+    $(".playagain").removeClass("hidden");
+  } else if (
+    (gamespace[0].text() == "O" &&
+      gamespace[1].text() == "O" &&
+      gamespace[2].text() == "O") ||
+    (gamespace[3].text() == "O" &&
+      gamespace[4].text() == "O" &&
+      gamespace[5].text() == "O") ||
+    (gamespace[6].text() == "O" &&
+      gamespace[7].text() == "O" &&
+      gamespace[8].text() == "O") ||
+    (gamespace[0].text() == "O" &&
+      gamespace[3].text() == "O" &&
+      gamespace[6].text() == "O") ||
+    (gamespace[1].text() == "O" &&
+      gamespace[4].text() == "O" &&
+      gamespace[7].text() == "O") ||
+    (gamespace[2].text() == "O" &&
+      gamespace[5].text() == "O" &&
+      gamespace[8].text() == "O") ||
+    (gamespace[0].text() == "O" &&
+      gamespace[4].text() == "O" &&
+      gamespace[8].text() == "O") ||
+    (gamespace[2].text() == "O" &&
+      gamespace[4].text() == "O" &&
+      gamespace[6].text() == "O")
   ) {
-    runBot = false;
-    bot(runBot);
-    setTimeout(() => {
-      resultBox.classList.add("show");
-      playBoard.classList.remove("show");
-    }, 700);
-    wonText.innerHTML = `Player ${playerSign}<br> won the game!`;
-  } else {
-    if (
-      getIdVal(1) != "" &&
-      getIdVal(2) != "" &&
-      getIdVal(3) != "" &&
-      getIdVal(4) != "" &&
-      getIdVal(5) != "" &&
-      getIdVal(6) != "" &&
-      getIdVal(7) != "" &&
-      getIdVal(8) != "" &&
-      getIdVal(9) != ""
-    ) {
-      runBot = false;
-      bot(runBot);
-      setTimeout(() => {
-        resultBox.classList.add("show");
-        playBoard.classList.remove("show");
-      }, 700);
-      wonText.textContent = "Match has been drawn!";
-    }
+    isPlayer = true;
+    isStarted = false;
+    display.text("O wins!");
+    $(".playagain").removeClass("hidden");
+  } else if (
+    gamespace[0].text() != "" &&
+    gamespace[1].text() != "" &&
+    gamespace[2].text() != "" &&
+    gamespace[3].text() != "" &&
+    gamespace[4].text() != "" &&
+    gamespace[5].text() != "" &&
+    gamespace[6].text() != "" &&
+    gamespace[7].text() != "" &&
+    gamespace[8].text() != ""
+  ) {
+    isPlayer = true;
+    isStarted = false;
+    display.text("Draw!");
+    $(".playagain").removeClass("hidden");
   }
 }
-replayBtn.onclick = () => {
-  window.location.reload();
-};
+let cpuChoice;
+function cpuMakingChoice() {
+  cpuChoice = Math.floor(Math.random() * 9);
+  if (gamespace[cpuChoice].text() == "") {
+    gamespace[cpuChoice].text(playercode);
+    toggleplayer();
+    checkwin();
+  }
+}
+let cpuTurn = window.setInterval(function () {
+  if (isPlayer == false) {
+    cpuMakingChoice();
+  }
+}, 500);
+block.click(function () {
+  if ($(this).text() == "" && isPlayer && isStarted) {
+    $(this).text(playercode);
+    toggleplayer();
+    checkwin();
+  }
+});
+function resetEverything() {
+  playercode = "";
+  block.text("");
+  display.text("Choose Your Letter");
+  $(".playerchoice").removeClass("hidden");
+  $(".playagain").addClass("hidden");
+  $(".game-grid").addClass("hidden");
+  isStarted = false;
+}
+$("#playagainyes").click(function () {
+  console.log("Resetting variables and restarting the game.");
+  resetEverything();
+});
